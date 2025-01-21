452c2e0c9611d1a165fa4ee6fd82ae8dc745753e
Kapil Jangid <kapiljangid040@gmail.com>
2025-01-22 01:02:35 +0530

    refactor template fetching logic; replace static HTML with dynamic template loading from file

--- app/src/components/main/templates/all-templates.tsx
+++ app/src/components/main/templates/all-templates.tsx
@@ -1,28 +1,20 @@
 import { useEffect, useState } from "react";
 import TemplateCard from "./template-card";
-import { getAllTemplates } from "@/services/template.services";
+import { getAllTemplates, getTemplate } from "@/services/template.services";
 import { Template } from "@/lib/types/index.types";
 
 const AllTemplates = () => {
-  const [templates, setTemplates] = useState<Template[]>([]);
-  const fetchTemplates = async () => {
-    const res = await getAllTemplates();
-    setTemplates(res.templates);
+  const [template, setTemplate] = useState<Template | null>(null);
+  const fetchTemplate = async () => {
+    const res = await getTemplate();
+    setTemplate(res);
   };
   useEffect(() => {
-    fetchTemplates();
-  }, [templates]);
+    fetchTemplate();
+  }, [template]);
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 place-items-center gap-4 flex-wrap w-full h-full p-4">
-      {templates.length > 0 && templates ? (
-        templates.map((template: any, index: number) => (
-          <TemplateCard key={index} template={template} />
-        ))
-      ) : (
-        <div className="flex items-center justify-center w-full h-full col-span-12">
-          No Templates Found
-        </div>
-      )}
+      {template ? <TemplateCard template={template} /> : <p>Loading...</p>}
     </div>
   );
 };
