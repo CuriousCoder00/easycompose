b2aa08f7e3a7981f566df138508d9e500d4716bd
Kapil Jangid <kapiljangid040@gmail.com>
2025-01-22 23:03:19 +0530

    refactor UserTemplates component to simplify TemplateCard rendering

--- app/src/components/main/dashboard/user-templates.tsx
+++ app/src/components/main/dashboard/user-templates.tsx
@@ -16,9 +16,7 @@ const UserTemplates = () => {
   return (
     <div className="flex flex-col items-start justify-start gap-4 flex-wrap w-full p-4">
       {templates.length > 0 && templates ? (
-        templates.map((template: any, index: number) => (
-          <TemplateCard key={index} template={template} />
-        ))
+        templates.map(() => <TemplateCard />)
       ) : (
         <div className="flex flex-col items-center justify-center w-40 h-40 rounded border border-border hover:scale-105 transition-all cursor-pointer hover:bg-gradient-to-br hover:from-sky-700/20 hover:to-sky-700/5 duration-300">
           <span>
